b7420fdf7a92957d44c26480aacb770112641aa0
kingieX <chimakingsley216@gmail.com>
2023-03-28 23:48:16 +0100

    Make web page responsive

---
+++ interface/script.js
@@ -0,0 +1,6 @@
+// side bars
+var menu = document.querySelector('.menu');
+var menuBtn = document.querySelector('.menu .nav');
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('opened')
+});
